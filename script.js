790d04e2d707c5f5cbd604e2a41da855eb620ca6
Lindllay <maciejsemeniuk95@gmail.com>
2022-08-26 05:52:33 +0200

    added animation and delete functionality (only in view atm)

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const overlay = document.querySelector(".overlay");
 const btnModalCancel = document.querySelector(".btn--form-grey");
 const btnModalDelete = document.querySelector(".btn--form-red");
 const itemContainer = document.querySelector(".section");
+let state;
+let ID = 1;
+let deleteID;
+let itemType;
+
+const timeout = function (item) {
+	setTimeout(function () {
+		item.nextElementSibling.remove();
+		item.remove();
+	}, 1000);
+};
 
 const renderData = function (data) {
 	data.comments.forEach((comment) => {
+		ID = ID < comment.id ? comment.id : ID;
 		let replyMarkup = "";
 		comment.replies.forEach((reply) => {
-			replyMarkup += `<div class="comment answer-parent">
+			ID = ID < reply.id ? reply.id : ID;
+
+			replyMarkup += `<div class="comment answer-parent" data-id="${
+				reply.id
+			}">
             <div class="comment__vote">
               <span class="comment__vote--button">+</span
               ><span class="comment__vote--rating">${reply.score}</span
@@ -49,7 +65,7 @@ const renderData = function (data) {
             <div class="btn-container">
             ${
 				reply.user.username === data.currentUser.username
-					? `<div class="btn-icon-box delete-box">
+					? `<div class="btn-icon-box delete-box delete-box--reply">
                   <div class="icon icon--red icon--delete"></div>
                   <a href="#" class="btn btn--small btn--red"
                       >Delete</a
@@ -90,11 +106,11 @@ const renderData = function (data) {
 			</form>
 		</div>`;
 		});
-		const commentMarkup = `<div class="item">
+		const commentMarkup = `<div class="item" data-id="${comment.id}">
         <div class="comment answer-parent">
             <div class="comment__vote">
                 <span class="comment__vote--button">+</span
-                ><span class="comment__vote--rating">0</span
+                ><span class="comment__vote--rating">${comment.score}</span
                 ><span class="comment__vote--button">–</span>
             </div>
             <div class="comment__content">
@@ -117,7 +133,7 @@ const renderData = function (data) {
             <div class="btn-container">
             ${
 				comment.user.username === data.currentUser.username
-					? `<div class="btn-icon-box delete-box">
+					? `<div class="btn-icon-box delete-box delete-box--comment">
                   <div class="icon icon--red icon--delete"></div>
                   <a href="#" class="btn btn--small btn--red"
                       >Delete</a
@@ -130,15 +146,14 @@ const renderData = function (data) {
                   >
               </div>`
 					: `<div class="btn-icon-box reply-box">
-          <div class="icon icon--blue icon--reply"></div>
-          <a href="#" class="btn btn--small btn--blue"
-              >Reply</a>
-        </div>`
+                        <div class="icon icon--blue icon--reply"></div>
+                            <a href="#" class="btn btn--small btn--blue">Reply</a>
+                        </div>`
 			}
-			</div>
-        </div>
-        <!-- Reply form -->
-<div class="answer display-none">
+			        </div>
+                 </div>
+             <!-- Reply form -->
+        <div class="answer display-none">
             <div class="answer__img">
                 <img
                     src="${data.currentUser.image.png}"
@@ -146,21 +161,20 @@ const renderData = function (data) {
                     class="answer__avatar--img"
                 />
             </div>
-            <form class="answer__form">
-                <textarea
-                    type="text"
-                    placeholder="Add a comment..."
-                    class="input"
-                ></textarea>
+                <form class="answer__form">
+                    <textarea
+                      type="text"
+                      placeholder="Add a comment..."
+                     class="input"></textarea>
                 <button class="btn btn--big btn--reply">Reply</button>
-            </form>
-        </div>
-    </div>
-<div class="replies">
-<div class="replies__border"></div>
-<div class="replies__container">
-${replyMarkup}</div>
-</div>`;
+                 </form>
+             </div>
+            </div>
+        <div class="replies">
+        <div class="replies__border"></div>
+    <div class="replies__container">
+    ${replyMarkup}</div>
+            </div>`;
 
 		itemContainer.insertAdjacentHTML("beforeend", commentMarkup);
 	});
@@ -187,8 +201,69 @@ ${replyMarkup}</div>
             Send
         </button>
     </form>
-</div>`;
+        </div>`;
 	itemContainer.insertAdjacentHTML("beforeend", bottomTextarea);
+};
+
+const newComment = function (curUser, text) {
+	ID++;
+	const commentData = {
+		id: ID,
+		content: text,
+		createdAt: "now",
+		replies: [],
+		score: 0,
+		user: {
+			image: curUser.image.png,
+			username: curUser.username,
+		},
+	};
+
+	state.comments.push(commentData);
+};
+
+const ReplyOnComment = function (data, text, elementID) {
+	ID++;
+	const target = data.comments.find((comment) => comment.id == elementID);
+
+	const replyData = {
+		id: ID,
+		content: text,
+		createdAt: "now",
+		replyingTo: "",
+		score: 0,
+		user: {
+			image: data.currentUser.image.png,
+			username: data.currentUser.username,
+		},
+	};
+	target.replies.push(replyData);
+};
+
+const ReplyOnReply = function (data, text, elementID) {
+	ID++;
+	let target;
+	data.comments.forEach((comment) => {
+		comment.replies.forEach((reply) => {
+			if (reply.id == elementID) {
+				target = comment;
+			}
+		});
+	});
+	console.log(target);
+
+	const replyData = {
+		id: ID,
+		content: text,
+		createdAt: "now",
+		replyingTo: "",
+		score: 0,
+		user: {
+			image: data.currentUser.image.png,
+			username: data.currentUser.username,
+		},
+	};
+	target.replies.push(replyData);
 };
 
 //// Data from JSON file
@@ -196,10 +271,10 @@ ${replyMarkup}</div>
 const getJSONData = async function () {
 	try {
 		const res = await fetch("data.json");
-		const data = await res.json();
-		console.log(data);
+		state = await res.json();
+		console.log(state);
 
-		renderData(data);
+		renderData(state);
 	} catch (err) {
 		console.log(err);
 	}
@@ -231,8 +306,9 @@ document.addEventListener("click", function (e) {
 	if (btnReply || btnSend) {
 		const text = e.target.previousElementSibling.value;
 		const form = e.target.closest(".answer");
+		// const commentID =
 
-		const newItemMarkup = `<div class="item">
+		const newItemMarkup = `<div class="item" data-id="${ID + 1}">
         <div class="comment answer-parent">
             <div class="comment__vote">
                 <span class="comment__vote--button">+</span
@@ -257,7 +333,7 @@ document.addEventListener("click", function (e) {
                 </div>
             </div>
             <div class="btn-container">
-				<div class="btn-icon-box delete-box">
+				<div class="btn-icon-box delete-box delete-box--comment">
 					<div
 						class="icon icon--red icon--delete"
 					></div>
@@ -297,8 +373,10 @@ document.addEventListener("click", function (e) {
 <div class="replies">
 <div class="replies__border"></div>
 <div class="replies__container"></div>
-</div>`;
-		const newReplyMarkup = `<div class="comment answer-parent">
+            </div>`;
+		const newReplyMarkup = `<div class="comment answer-parent" data-id="${
+			ID + 1
+		}">
         <div class="comment__vote">
           <span class="comment__vote--button">+</span
           ><span class="comment__vote--rating">2</span
@@ -335,7 +413,7 @@ document.addEventListener("click", function (e) {
           </form> -->
         </div>
         <div class="btn-container">
-          <div class="btn-icon-box delete-box">
+          <div class="btn-icon-box delete-box delete-box--reply">
             <div
               class="icon icon--red icon--delete"
             ></div>
@@ -352,7 +430,7 @@ document.addEventListener("click", function (e) {
             >
           </div>
         </div>
-      </div>`;
+            </div>`;
 
 		const formMarkup = `<div class="answer ${
 			btnReply ? "display-none" : ""
@@ -374,14 +452,17 @@ document.addEventListener("click", function (e) {
             </form>
             </div>`;
 
-		// IF SEND BUTTON CLICKED (NEW COMMENT)
+		// NEW COMMENT
 		if (btnSend) {
 			itemContainer.insertAdjacentHTML("beforeend", newItemMarkup);
 			form.remove();
 			itemContainer.insertAdjacentHTML("beforeend", formMarkup);
+
+			newComment(state.currentUser, text);
+			console.log(state);
 		}
 
-		// IF BIG REPLY BUTTON CLICKED on comment
+		// REPLY ON COMMENT
 		if (e.target.closest(".item")) {
 			const repliesContainer = e.target
 				.closest(".item")
@@ -392,20 +473,59 @@ document.addEventListener("click", function (e) {
 			form.querySelector(".input").value = "";
 			repliesContainer.insertAdjacentHTML("beforeend", formMarkup);
 
+			const targetID = e.target.closest(".item").dataset.id;
+
+			ReplyOnComment(state, text, targetID);
+
 			e.target.closest(".item").querySelector(".input").style.height =
 				"8rem";
 		}
-		// IF BIG REPLY BUTTON CLICKED on reply
+		// REPLY ON REPLY
 		if (e.target.closest(".replies__container")) {
 			const repliesContainer = e.target.closest(".replies__container");
-			const input = repliesContainer.querySelector(".input");
 
 			repliesContainer.insertAdjacentHTML("beforeend", newReplyMarkup);
 			form.classList.toggle("display-none");
 			form.querySelector(".input").value = "";
 			repliesContainer.insertAdjacentHTML("beforeend", formMarkup);
 
-			input.style.height = "8rem";
+			const targetID =
+				e.target.closest(".answer").previousElementSibling.dataset.id;
+
+			ReplyOnReply(state, text, targetID);
+		}
+	}
+
+	///////// DELETE COMMENTS ////////
+
+	// DELETE COMMENT
+	if (e.target.closest(".delete-box--comment")) {
+		const comment = e.target.closest(".item");
+		const commentID = comment.dataset.id;
+		deleteID = commentID;
+		itemType = "comment";
+	}
+	// DELETE REPLY
+	if (e.target.closest(".delete-box--reply")) {
+		const reply = e.target.closest(".answer-parent");
+		const targetID = reply.dataset.id;
+		deleteID = targetID;
+		itemType = "reply";
+	}
+
+	if (e.target == btnModalDelete) {
+		overlay.classList.add("hidden");
+		modal.classList.add("hidden");
+
+		if (itemType === "comment") {
+			timeout(document.querySelector(`.item[data-id="${deleteID}"]`));
+			console.log(deleteID);
+		}
+		if (itemType === "reply") {
+			timeout(
+				document.querySelector(`.answer-parent[data-id="${deleteID}"]`)
+			);
+			console.log(deleteID);
 		}
 	}
 
